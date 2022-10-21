1dfa68774431323a1d3b7f2741948d39dc38156c
unknown <engr.mohammadjannatulnayeem@gmail.com>
2022-10-21 16:47:06 +0600

    code uploaded

---
+++ src/DoWhileLoopDemo/DoWhileLoopDemo.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DoWhileLoopDemo;
+
+/**
+ *
+ * @author nayeem
+ */
+public class DoWhileLoopDemo {
+    public static void main(String[]args)
+    {
+    
+        int x=1;
+        
+        do{
+        
+          System.out.println("Output is:"+x);
+          x++;
+        
+        
+        }
+        
+        while(x<=10);
+    
+    
+    }
+    
+}
